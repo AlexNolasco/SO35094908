238c92dc9e58bdc646638ef62245eed04910ca78
Alex Nolasco <alexn@gmed.com>
2016-01-29 19:40:09 -0500

    First commit

---
+++ Service.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace PUendelig
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service : IService
+    {
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "AddNewActivity/")]
+        public Activity AddActivity(Activity activity)
+        {
+            if (activity == null)
+            {
+                throw new ArgumentNullException("activity");
+            }
+            return activity;
+        }
+    }
+}
